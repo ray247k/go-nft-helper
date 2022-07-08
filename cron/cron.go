4ed119e2b97b6081afc8eccac12188e481e2d0be
Ray Cheng <ray.cheng@getoken.io>
2022-07-08 15:39:13 +0800

    Add cron

---
+++ cron/cron.go
@@ -0,0 +1,52 @@
+package cron
+
+import (
+	"log"
+	"strconv"
+	"time"
+
+	"egox/pkg/get_nft_owner_of"
+	"egox/pkg/get_nft_token_uri"
+	"egox/pkg/get_nft_total_supply"
+
+	"github.com/robfig/cron/v3"
+)
+
+func Cronjob() {
+	log.Println("Cron Starting...")
+
+	c := cron.New(cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)))
+
+	c.AddFunc("* * * * *", func() {
+		getItemsOwnerOfByContract("0x4e2Fbc9e3feB25999991C249217d8ee5608860AD")
+	})
+	c.Start()
+	time.Sleep(time.Minute * 5) // 5 分鐘會自己停
+}
+
+func getItemsOwnerOfByContract(contract string) {
+	totalSupply, err := get_nft_total_supply.GetTotalSupply(contract)
+	if err != nil {
+		log.Printf("Get NFT total supply failed, err: %v\n", err)
+	}
+
+	log.Printf("NFT total supply: %s", totalSupply)
+
+	totalAmount, _ := strconv.Atoi(totalSupply.String())
+
+	for i := 1; i <= totalAmount; i++ {
+		owner, err := get_nft_owner_of.GetOwnerOf("0x4e2fbc9e3feb25999991c249217d8ee5608860ad", i)
+		if err != nil {
+			log.Printf("Get NFT owner of failed, err: %v\n", err)
+		}
+
+		tokenURI, err := get_nft_token_uri.GetNftTokenUri("0x4e2fbc9e3feb25999991c249217d8ee5608860ad", i)
+
+		if err != nil {
+			log.Printf("Get NFT tokenURI failed, err: %v\n", err)
+		}
+
+		log.Printf("Token Id: %v owner: %s tokenURI: %s", i, owner, *tokenURI)
+
+	}
+}
--- main.go
+++ main.go
@@ -1,40 +1,10 @@
 package main
 
 import (
-	"egox/pkg/get_nft_owner_of"
-	"egox/pkg/get_nft_token_uri"
-	"egox/pkg/get_nft_total_supply"
-	"log"
-	"strconv"
+	"egox/cron"
 )
 
 func main() {
-	getItemsOwnerOfByContract("0x4e2Fbc9e3feB25999991C249217d8ee5608860AD")
-}
-
-func getItemsOwnerOfByContract(contract string) {
-	totalSupply, err := get_nft_total_supply.GetTotalSupply(contract)
-	if err != nil {
-		log.Printf("Get NFT total supply failed, err: %v\n", err)
-	}
-
-	log.Printf("NFT total supply: %s", totalSupply)
-
-	totalAmount, _ := strconv.Atoi(totalSupply.String())
-
-	for i := 1; i <= totalAmount; i++ {
-		owner, err := get_nft_owner_of.GetOwnerOf("0x4e2fbc9e3feb25999991c249217d8ee5608860ad", i)
-		if err != nil {
-			log.Printf("Get NFT owner of failed, err: %v\n", err)
-		}
-
-		tokenURI, err := get_nft_token_uri.GetNftTokenUri("0x4e2fbc9e3feb25999991c249217d8ee5608860ad", i)
-
-		if err != nil {
-			log.Printf("Get NFT tokenURI failed, err: %v\n", err)
-		}
-
-		log.Printf("Token Id: %v owner: %s tokenURI: %s", i, owner, *tokenURI)
-
-	}
+	// TODO 改成獨立執行序一直跑的版本
+	cron.Cronjob()
 }
