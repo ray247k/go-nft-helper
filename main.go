0ddd3e904115f10d5f9b215c6f84fac539db1325
Ray Cheng <ray.cheng@getoken.io>
2022-07-25 11:14:07 +0800

    Change remove cron package

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"go-nft-helper/cron"
+	"go-nft-helper/database/model"
+	"go-nft-helper/pkg/get_nft_owner_of"
+	"go-nft-helper/pkg/get_nft_token_uri"
+	"go-nft-helper/pkg/get_nft_total_supply"
+	"log"
+	"strconv"
+	"time"
 )
 
 func main() {
-	cron.Cronjob()
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		contracts := model.GetAllContracts()
+		for _, contract := range contracts {
+			getItemsOwnerOfByContract(contract)
+		}
+	}
+}
+
+func getItemsOwnerOfByContract(contract string) {
+	totalSupply, err := get_nft_total_supply.GetTotalSupply(contract)
+	if err != nil {
+		log.Printf("Get NFT total supply failed, err: %v\n", err)
+	}
+
+	log.Printf("Contract: %s total supply: %s", contract, totalSupply)
+
+	totalAmount, _ := strconv.Atoi(totalSupply.String())
+
+	for i := 1; i <= totalAmount; i++ {
+		owner, err := get_nft_owner_of.GetOwnerOf(contract, i)
+		if err != nil {
+			log.Printf("Get NFT owner of failed, err: %v\n", err)
+		}
+
+		tokenURI, err := get_nft_token_uri.GetNftTokenUri(contract, i)
+
+		if err != nil {
+			log.Printf("Get NFT tokenURI failed, err: %v\n", err)
+		}
+
+		log.Printf("Token Id: %v owner: %s tokenURI: %s", i, owner, *tokenURI)
+	}
 }
